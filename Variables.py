15efc64e4c33da52f387cfa924d6742eaeb99c30
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-06-14 13:06:43 +0530

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,23 @@
+x = 34
+y = "John"
+
+print(x)
+print(y)
+
+x = 34
+print(x)
+x = "sally"
+print(x)
+
+# Casting
+x1 = int(34)
+x2 = str(34)
+x3 = float(34)
+print(x1)
+print(x2)
+print(x3)
+
+# Get the type
+print(type(x1))
+print(type(x2))
+print(type(x3))
