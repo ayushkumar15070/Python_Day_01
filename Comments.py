b8e7a2b932c9508ae850411bf730537284b61ac7
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-06-14 12:28:09 +0530

    Create Comments.py

---
+++ Comments.py
@@ -0,0 +1,10 @@
+# Hello, this is the comment which don't do anything in the code but they are just used to explain or let us keep the notes of what we have created,
+# for example if we are creating a software in which there are 4 other fellow programmer so the code should be understandable by them also so we use the comments
+# the above are single line comments 
+
+'''
+this is the multiline comment
+this is the multiline comment
+this is the multiline comment
+this is the multiline comment
+'''
