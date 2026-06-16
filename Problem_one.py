569c21ac90eb0de7c61d47e577aa1d282be0b136
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-06-16 13:23:19 +0530

    Problem_one

---
+++ Problem_one.py
@@ -0,0 +1,13 @@
+Name = "Rahul"
+Age = 19
+Height = 5.8
+Is_Hostel_Student = True
+
+print(f"""
+-------STUDENT CARD-------
+    Name: {Name}
+    Age: {Age}
+    Height: {Height}
+    Is Hostel Student: {Is_Hostel_Student}
+""")
+
