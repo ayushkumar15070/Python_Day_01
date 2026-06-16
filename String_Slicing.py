f13ac7c996380c95bdd4d97d5e4e4c2be54bdc65
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-06-16 12:08:41 +0530

    Slicing

---
+++ String_Slicing.py
@@ -0,0 +1,14 @@
+# Slicing the strings in range
+Cricketer = "Here is the number one batsman Virat Kohli"
+print(Cricketer[2:7])
+
+#Slicing from starting 
+
+print(Cricketer[:8])
+
+
+# Slicing to the end
+print(Cricketer[4:])
+
+#Negative indexing
+print(Cricketer[-42:-1])
