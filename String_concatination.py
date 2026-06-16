7f27b153cd77442944ae787d20f3f5bb7b376c19
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-06-16 12:24:04 +0530

    Create String_concatination.py

---
+++ String_concatination.py
@@ -0,0 +1,9 @@
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+a = "Hello this whole journey or the beginning of my python learning, and yeah i am lil bit weak in english but it will be improved after years of hardwork and improvement"
+b = "And also i am learning this basically to build something more valuable to the world so that it could contribute to the humanity and is being used to make better world"
+c = a + " " + b
+print(c)
