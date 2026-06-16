5c610ff8faecb659495bcaa84abccf9e473e8ab3
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-06-16 12:44:29 +0530

    Create Escape_character.py

---
+++ Escape_character.py
@@ -0,0 +1,3 @@
+string = "Hello everyone i am Ayush Kumar Vishwakarma \" and \" i am now \n becoming \t the number one\\ python programmer\b the humanity has\f ever witnessed and its my promise and i am going to be the best in the world!!!"
+
+print(string)
