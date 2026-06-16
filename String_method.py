63f499e4aeae36b0f34cbf584436a17b03fde19a
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-06-16 12:58:48 +0530

    Create String_method.py

---
+++ String_method.py
@@ -0,0 +1,9 @@
+string = "hello everyone i am Ayush Kumar Vishwakarma and i am learning the python programming language and i am trying to become the number one programmer in the world and also one thing is for sure that i am going to be the best in the world and yeah that's not the statement that's the reality. The world's best programmer the humanity will ever witnessed, i am going to create the AGI that are going to change the world like ultron which can go and hack into any system and can get the details but it will not used for the bad purpose it will be used for good purposes\n"
+
+print(string.capitalize())
+print(string.casefold())
+print(string.center(34))
+print(string.count("Ayush"))
+print(string.encode())
+print(string.endswith("ayush"))
+print(string.expandtabs(23))
