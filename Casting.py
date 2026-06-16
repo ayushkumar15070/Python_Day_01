703ff6b1e1b43dfd9315c60609e5994bf52f08f4
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-06-16 11:40:59 +0530

    Create Casting.py

---
+++ Casting.py
@@ -0,0 +1,39 @@
+#Integer Data type
+x1 = int(2)
+x2 = int(2.343)
+x3 = int("3")
+
+print(x1)
+print(x2)
+print(x3)
+print(type(x1))
+print(type(x2))
+print(type(x3))
+
+#Float Data type
+y1 = float(2)
+y2 = float(3.45)
+y3 = float("3.455")
+y4 = float("3")
+
+print(y1)
+print(y2)
+print(y3)
+print(y4)
+print(type(y1))
+print(type(y2))
+print(type(y3))
+print(type(y4))
+
+
+#String Data type
+z1 = str("Ayush")
+z2 = str(4)
+z3 = str(4.3)
+
+print(z1)
+print(z2)
+print(z3)
+print(type(z1))
+print(type(z2))
+print(type(z3))
