738d61b9bed1ebd5d338d9fa5cb031ca80d3498d
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-06-17 12:31:45 +0530

    Update Operators.py

--- Operators.py
+++ Operators.py
@@ -9,3 +9,13 @@ print(x / y)
 print(x % y)
 print(x ** y)
 print(x // y)
+
+x = 5
+x = x & 3
+print(x)
+x = x | 3
+print(x)
+# Walrus operator
+if (length := len(input("Enter your name: "))) > 5:
+    print("Long name")
+    print(length)
