4fda53317f8052d4ef86a3af691a26743edf58b5
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-06-17 13:09:46 +0530

    Create Bitwise_operator.py

---
+++ Bitwise_operator.py
@@ -0,0 +1,21 @@
+print( 6 & 3)
+
+# 0110 = 6
+# 0011 = 3
+
+# answer = 0010 which is 2 that's why it is returning the answer or output as 2
+
+
+print(6 | 3)
+
+# 0110 = 6
+# 0011 = 3
+
+#answer = 0111 which is 7 that's why it is returning the answer or output as 7
+
+print(6 ^ 3)
+
+# 0110 = 6
+# 0011 = 3
+
+#answer = 0101 which is 5 that's why it is returning the answer or output as 5
