28ebd332f3026eeababbb945db9254f8399e9d5d
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-06-17 12:39:55 +0530

    Create Ternary_Operator.py

---
+++ Ternary_Operator.py
@@ -0,0 +1,23 @@
+#Ternary operator means doing the if else in one line or shorter lines 
+
+x = 5
+
+y = "Greater than 4" if x > 4 else "Smaller than 4"
+
+print(y)
+
+
+x = int(input("Enter any number: "))
+ 
+num = "Greater than 10" if x > 10 else "Smaller than 10"
+
+print(num)
+
+x = int(input("Enter the number 5: "))
+
+number = "You pressed the key what we told to you" if x == 5 else "You pressed the wrong key" 
+print(number)
+
+number2 = "You pressed the right key" if x == 5 else "You pressed the wrong key"
+
+print(number2)
