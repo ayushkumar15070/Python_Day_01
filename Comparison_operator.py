2023e9a4e0070cfe92b3f12e53d7db3f826c34b4
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-06-17 12:43:34 +0530

    Create Comparison_operator.py

---
+++ Comparison_operator.py
@@ -0,0 +1,17 @@
+# Comparison operator used to compare between two variables 
+x = 4
+y = 3
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+
+#Chaining comparison operator
+
+x = 5
+print(1 < x < 10)
+print( 1 < x and 10 > x)
