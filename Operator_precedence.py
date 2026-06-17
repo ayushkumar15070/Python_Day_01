0593ed9d5253254b5e747c545f28de41fa8f343f
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-06-17 13:13:06 +0530

    Create Operator_precedence.py

---
+++ Operator_precedence.py
@@ -0,0 +1,7 @@
+print((2 + 3) - (3 + 4))
+
+# Here multiplication has a higher precedence that's why it will first do multiplication then addition
+print(100 + 34 * 4)
+
+#if two operators having the same precedence then it will do operations from left to right
+print(4 + 5 - 3 + 5)
