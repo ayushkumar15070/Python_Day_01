493b9908fd3d1272957556589bbe79501a191a2f
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-06-18 12:45:50 +0530

    Update Python_List.py

--- Python_List.py
+++ Python_List.py
@@ -19,7 +19,6 @@ print(list4[2])
 
 #Negative indexing 
 print(list4[-2])
-
 #range 
 print(list4[1:3])
 
@@ -28,3 +27,26 @@ if name in list1:
     print(f"The Fruit {name} is present in the list {list1} at index of 0")
 else:
     print(f"The Fruit {name} is not present in the list {list1}")
+
+#Changing the values in the list
+
+cars = ["BMW", "Mercedes", "Audi", "Volvo", "TATA"]
+cars[2] = "Saburo"
+
+for i in range(len(cars)):
+    print(cars[i])
+else:
+    print("Nothing is there")
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
+print(thislist)
+
+
+#insert method
+
+Aeroplanes = ["Gulfstrem101", "Boeing777", "Gulfstream404", "NanoBanana"]
+fortuner = input("Enter any name of a fortuner car: ")
+Aeroplanes.insert(3, fortuner)
+print(Aeroplanes)
