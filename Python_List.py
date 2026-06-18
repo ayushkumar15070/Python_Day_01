9dc0720be07c3f7b46fc07506e51283696329a84
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-06-18 12:32:58 +0530

    Update Python_List.py

--- Python_List.py
+++ Python_List.py
@@ -15,3 +15,16 @@ print(type(list4))
 list5 = list(("Apple", "Banana", "Cherry"))
 print(list5)
 
+print(list4[2])
+
+#Negative indexing 
+print(list4[-2])
+
+#range 
+print(list4[1:3])
+
+name = input("Enter the name of a fruit: ")
+if name in list1:
+    print(f"The Fruit {name} is present in the list {list1} at index of 0")
+else:
+    print(f"The Fruit {name} is not present in the list {list1}")
