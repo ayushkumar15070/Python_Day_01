3c07980a352509163d5cb79250abe69203c8f854
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-06-18 12:25:04 +0530

    Create Python_List.py

---
+++ Python_List.py
@@ -0,0 +1,17 @@
+#List 
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+print(len(fruits))
+
+list1 = ["apple", "fruits", "cherry"]
+list2 = [1, 2, 3, 4]
+list3 = [True, False, True, False]
+
+
+list4 = ["apple", 34, "reddish", "Kashmiri"]
+print(type(list4))
+
+list5 = list(("Apple", "Banana", "Cherry"))
+print(list5)
+
