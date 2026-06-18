984f4f5225c83878243a36c0d9bc20350b0e0608
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-06-18 15:55:33 +0530

    Update Loop_lists.py

--- Loop_lists.py
+++ Loop_lists.py
@@ -13,3 +13,38 @@ while i < len(Thislist):
 
 #Loop using loop Comprehension
 [print(x) for x in Thislist]
+
+
+
+fruits = ["Apple", "Banana", "Cherry"]
+newfruits = []
+
+for x in fruits:
+    if "a" in x:
+        newfruits.append(x)
+
+print(newfruits)
+
+
+# we can do the above operation within one line 
+
+newlist = [x for x in fruits if "a" in x]
+print(newlist)
+
+# syntax => newlist = [expression for item in iterable if condition == True]
+
+newlist2 = [x for x in fruits if x != "Apple"]
+print(newlist2)
+
+# list1 = [x for x in fruits]
+list1 = [x for x in range(10)]
+list2 = [x for x in range(10) if x > 3]
+list3 = [x.upper() for x in fruits]
+list4 = ["Hello" for x in fruits]
+list5 = [x if x != "Banana" else "orange" for x in fruits]
+print(list5)
+print(list4)
+print(list3)
+print(list2)
+
+print(list1)
