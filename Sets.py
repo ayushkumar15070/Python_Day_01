31d81dba05b26d6483ce5672bd0f6b2493ff8412
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-06-21 12:23:18 +0530

    Update Sets.py

--- Sets.py
+++ Sets.py
@@ -21,3 +21,17 @@ if "cars" not in set1:
     print("No, the cars is not present in set1")
 else:
     print("Yes, the cars is present in set1")
+
+
+#Addint an item inside the set
+
+set4 = {"Apple", "Banana", "Cherry"}
+newitem = input("Enter the item name which you want to add in the set: ")
+set4.add(newitem)
+print(set4)
+
+
+#Adding the items of another set to the set
+set5 = {"Pineapple", "Guava", "Grapes"}
+set4.update(set5)
+print(set4)
