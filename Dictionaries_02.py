ef1065b6b2920ba8be4f4395b58b9e5ca623ca2a
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-06-28 12:00:16 +0530

    Update Dictionaries_02.py

--- Dictionaries_02.py
+++ Dictionaries_02.py
@@ -17,10 +17,27 @@ newcar = {
 
 }
 
-name = input("Enter the car name: ") 
-model = input("Enter the model name: ")
-year = int(input("Enter the year of the car: "))
-color = input("Enter the color of the car: ")
+# name = input("Enter the car name: ") 
+# model = input("Enter the model name: ")
+# year = int(input("Enter the year of the car: "))
+# color = input("Enter the color of the car: ")
 
-newcar.update({"Brand" : name, "Model": model, "year" : year, "color": color})
-print(newcar)
+# newcar.update({"Brand" : name, "Model": model, "year" : year, "color": color})
+# print(newcar)
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+thisdict.pop("model")
+print(thisdict)
+
+thisdict.popitem()
+print(thisdict)
+
+del thisdict["brand"]
+print(thisdict)
+
