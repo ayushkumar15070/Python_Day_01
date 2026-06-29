f9d44a018e8a35d4920a9aedc14565e02f2cf2d4
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-06-29 12:55:37 +0530

    Update Match_statements.py

--- Match_statements.py
+++ Match_statements.py
@@ -26,3 +26,14 @@ match day:
     print("Today is a weekday")
   case 6 | 7:
     print("I love weekends!")
+
+
+month = 4
+day = 4
+match day:
+  case 1 | 2 | 3 | 4 | 5 if month == 4:
+    print("A weekday in April")
+  case 1 | 2 | 3 | 4 | 5 if month == 5:
+    print("A weekday in May")
+  case _:
+    print("No match")
