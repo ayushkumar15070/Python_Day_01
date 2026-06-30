8d9a06d3394a33865e2612e1e9cbfe6d1174249a
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-06-30 11:31:47 +0530

    Create While_Loop.py

---
+++ While_Loop.py
@@ -0,0 +1,17 @@
+i = 1
+while i:
+    # print(i)
+    # print("Ayush is the best")
+    num = int(input("Guess the number: "))
+    if (num == 230000):
+        print("Yes you guessed the right number...!")
+        break
+    i += 1
+
+j = 0 
+while j < 10:
+    j = j + 1
+    if j == 3:
+        continue
+    print(j)
+    
