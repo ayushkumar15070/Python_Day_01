994276adc3016b8dfcae4f6e3fb1591ae94d4627
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-07-01 11:44:36 +0530

    Update Functions_01.py

--- Functions_01.py
+++ Functions_01.py
@@ -3,3 +3,24 @@ def greeting(name):
 
 name = input("Enter your name: ")
 print(greeting(name))
+
+
+
+def greetings(fname, lname, mname = ""):
+    if(mname == ""):
+        return f"Hello {fname} {lname}"
+    else:
+        return f"Hello {fname} {mname} {lname}"
+    
+
+fname = input("Enter your first name: ")
+mname = input("Enter your middle name: ")
+lname = input("Enter your last name: ")
+
+if(mname == ""):
+    sentence = greetings(fname, lname)
+    print(sentence)
+else:
+    sentence = greetings(fname, lname, mname)
+    print(sentence)
+    
