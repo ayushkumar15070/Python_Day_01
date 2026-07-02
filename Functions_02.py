fa1337224053057d06ab60b4718541f6bd2f7672
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-07-02 11:40:42 +0530

    Update Functions_02.py

--- Functions_02.py
+++ Functions_02.py
@@ -11,22 +11,29 @@ def my_function(greeting, *names):
     print(greeting, name)
 
 my_function("Hello", "Emil", "Tobias", "Linus")
-
-
-number = int(input("Enter the size of number which you want to add: "))
-num = []
-for i in range(number):
-   numbers = int(input("Enter the number"))
-   num.append(numbers)
-
-newnum = tuple(num)
-
-def add(*newnum):
-   total = 0
-   for i in newnum:
-      total = total + newnum
-
-   return total
-
-print(add(newnum))
     
+
+def my_function2(*numbers):
+  if len(numbers) == 0:
+    return None
+  max_num = numbers[0]
+  for num in numbers:
+    if num > max_num:
+      max_num = num
+  return max_num
+
+print(my_function2(3, 7, 2, 9, 1))
+
+
+def function(**dog):
+  print("The dog name is " + dog["name"])
+
+function(name = "Reo", anothername = "Leo")
+
+def my_function3(title, *args, **kwargs):
+  print("Title:", title)
+  print("Positional arguments:", args)
+  print("Keyword arguments:", kwargs)
+
+my_function3("User Info", "Emil", "Tobias", age = 25, city = "Oslo")
+
