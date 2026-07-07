b16fb650ad2b284e54e6c5ea520f4cddde6165e6
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-07-07 11:38:30 +0530

    Modules

--- Another_module.py
+++ Another_module.py
@@ -1,2 +1,9 @@
 def addition(num1, num2):
     print(num1 + num2)
+
+
+person2 = {
+    "Name": "Ayush", 
+    "Age" : 20,
+    "Hidden Talent": "Bike Riding"
+}
--- Modules_calling.py
+++ Modules_calling.py
@@ -8,17 +8,23 @@ print(Talent)
 print(mymodule.person1["Age"])
 
 
-#Renaming a module
+# Renaming a module
 
-# import Another_module as addition
+import Another_module as addition
 
 
-# num1 = int(input("Enter the number: "))
-# num2 = int(input("Enter the number: "))
-# addition.addition(num1, num2)
+num1 = int(input("Enter the number: "))
+num2 = int(input("Enter the number: "))
+addition.addition(num1, num2)
 
 
 import platform
 
 x =platform.processor()
 print(x)
+
+
+from Another_module import person2
+
+Hidden_Talent = person2["Hidden Talent"]
+print(Hidden_Talent)
