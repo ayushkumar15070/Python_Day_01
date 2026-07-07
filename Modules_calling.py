8b3f53cc7fd052e301ed5726cf7e066a6bc0f8db
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-07-07 11:34:15 +0530

    Modules

--- Modules_calling.py
+++ Modules_calling.py
@@ -7,3 +7,18 @@ Talent = mymodule.person1["Talent"]
 print(Talent)
 print(mymodule.person1["Age"])
 
+
+#Renaming a module
+
+# import Another_module as addition
+
+
+# num1 = int(input("Enter the number: "))
+# num2 = int(input("Enter the number: "))
+# addition.addition(num1, num2)
+
+
+import platform
+
+x =platform.processor()
+print(x)
