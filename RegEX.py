b025958e71031a959a90c60d4684e1a25127b799
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-07-10 12:51:12 +0530

    Create RegEX.py

---
+++ RegEX.py
@@ -0,0 +1,14 @@
+# A RegEX or regular expression is a sequence of characters that forms a seafch pattern
+
+import re
+
+txt = "The rain in Spain"
+x = re.search("The rain", txt)
+
+print(x)
+
+x = re.findall("ai", txt)
+print(x)
+
+
+print(x)
