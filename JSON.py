330cf4c8e38252bfa660a7f473295146de71e372
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-07-10 12:43:29 +0530

    Update JSON.py

--- JSON.py
+++ JSON.py
@@ -32,3 +32,20 @@ print(json.dumps(31.76))
 print(json.dumps(True))
 print(json.dumps(False))
 print(json.dumps(None))
+
+import json
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+print(json.dumps(x))
